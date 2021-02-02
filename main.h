16bd40ba3df110e1b255664708273782e840784b
NeutronX-dev <32855909+NeutronX-dev@users.noreply.github.com>
2021-02-02 16:29:07 -0500

    Imports (main.cpp)

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_DEF
+    #define MAIN_H_DEF
+    #define PRINT(x) std::cout << x << std::endl
+    // Default
+    #include <iostream>
+
+    // Data-types
+    #include <vector>
+    #include <string>
+
+    // Utilities
+    #include <thread>
+    #include <chrono>
+#endif
